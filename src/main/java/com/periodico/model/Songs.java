5637e449c9f9b5172cb20277d8a4b98d79fca8a1
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-04-10 12:18:49 +0200

    FEAT - created table DB structure in model

---
+++ src/main/java/com/periodico/model/Songs.java
@@ -0,0 +1,83 @@
+package com.periodico.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Songs")
+public class Songs{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private String lyrics;
+    private String genre;
+    private String artist;
+    private short release;
+    private String topic;
+
+    public Songs(){
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLyrics() {
+        return this.lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public short getRelease() {
+        return this.release;
+    }
+
+    public void setRelease(short release) {
+        this.release = release;
+    }
+
+    public String getTopic() {
+        return this.topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    }    
+}
+
---
+++ src/main/java/com/periodico/repository/PeriodicoRepository.java
@@ -0,0 +1,7 @@
+package com.periodico.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeriodicoRepository extends JpaRepository<> {
+    
+}
