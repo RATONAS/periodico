d38464418b0f531b6a35e3eabe0f359b23e80629
karisssha <krshmelendez@gmail.com>
2025-04-16 12:59:00 +0200

    added artists read update and delte methods

--- src/main/java/com/periodico/periodico/service/ArtistService.java
+++ src/main/java/com/periodico/periodico/service/ArtistService.java
@@ -1,5 +1,8 @@
 package com.periodico.periodico.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,25 +11,47 @@ import com.periodico.periodico.repository.ArtistRepository;
 
 @Service
 public class ArtistService {
-    private final ArtistRepository artistRepository;
+private final ArtistRepository artistRepository;
 
-    public ArtistService(ArtistRepository artistRepository){
-        this.artistRepository = artistRepository;
-    }
+public ArtistService(ArtistRepository artistRepository){
+this.artistRepository = artistRepository;
+}
 
-    public ResponseEntity<Object> createArtist(Artist artist){
-        artistRepository.save(artist);
-        return new ResponseEntity<>(artist, HttpStatus.CREATED);
-    }
+public ResponseEntity<Object> createArtist(Artist artist){
+artistRepository.save(artist);
+return new ResponseEntity<>(artist, HttpStatus.CREATED);
+}
+public List<Artist> getAllArtists() {
+return artistRepository.findAll();
+}
 
+public ResponseEntity<Object> getArtistById(int id) {
+Optional<Artist> artist = artistRepository.findById(id);
+if (artist.isPresent()) {
+return new ResponseEntity<>(artist.get(), HttpStatus.OK);
+} else {
+return new ResponseEntity<>("Artist not found", HttpStatus.NOT_FOUND);
+}
+}
+public Optional<Artist> updateArtist(int id, Artist artistDetails) {
+    return artistRepository.findById(id).map(artist -> {
+        artist.setArtistName(artistDetails.getArtistName());
+        artist.setCountry(artistDetails.getCountry());
+        artist.setIsActive(artistDetails.getIsActive());
+        return artistRepository.save(artist);
+    });
+}
 
-
-
-
+public ResponseEntity<Object> deleteArtist(int id) {
+Optional<Artist> optionalArtist = artistRepository.findById(id);
+if (optionalArtist.isPresent()) {
+artistRepository.delete(optionalArtist.get());
+return new ResponseEntity<>("Artist deleted successfully", HttpStatus.OK);
+} else {
+return new ResponseEntity<>("Artist not found", HttpStatus.NOT_FOUND);
+}
 }
 
 
 
-
-
-
+}
